1c438c3ef05623694bcf03c3fe4cadd6c860b2d2
zhangjinyuan <1337200048@qq.com>
2025-08-14 17:12:30 +0800

    first commit

---
+++ src/main/java/com/example/forum/ForumApplication.java
@@ -0,0 +1,14 @@
+package com.example.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumApplication {
+
+	public static void main(String[] args) {
+		//  tomcat and spring mvc
+		SpringApplication.run(ForumApplication.class, args);
+	}
+
+}
